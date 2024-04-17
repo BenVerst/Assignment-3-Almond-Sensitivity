b641194bd5b0908de4e43903783098056a78d9e6
BenVerst <Bjwversteeg@gmail.com>
2024-04-17 09:31:05 -0700

    updates

--- almond_profit.R
+++ almond_profit.R
@@ -1,8 +1,11 @@
 #' Function: almond_profit()
 #' Description: Calculates profit from almond yield anomoly
 #' Inputs:
-#'   @param 
-#'   @param
+#'   @param price_per_pound:
+#'   @param acre:
+#'   @param precip: total precipitation of January (millimeters)
+#'   @param min_temp: minimum daily temperature of February (degrees Celsius)
+#'  
 #' Output:
 #'   df:
 #'   
@@ -13,11 +16,13 @@
 
 source(almond_function.R)
 
-function(precip, min_temp, acre = 5){
+function(precip, min_temp, acre = 5, price_per_pound, clim = "data/clim.txt"){
+  
+  require(tidyverse)
   
   yield_anom <- almond_yield_fun(precip, min_temp)
   
-  (acre * 2000) + yield_anom * 
+  (acre * 1000 * price_per_pound) + yield_anom
   
   
 }
